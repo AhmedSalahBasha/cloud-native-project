d5ad41e3a54d8b1badf4398c9193696229bca59e
ahmed.basha <ahmed.basha@flixbus.com>
2022-10-06 19:38:03 +0200

    add handling exception for db connection

--- techtrends/app.py
+++ techtrends/app.py
@@ -20,18 +20,27 @@ logger = logging.getLogger(__name__)
 # global variable for counting db connections
 db_connection_count = 0
 
-# Function to get a database connection.
-# This function connects to database with the name `database.db`
+
 def get_db_connection():
+    """
+    Function to get a database connection.
+    This function connects to database with the name `database.db`
+    """
     global db_connection_count
-    connection = sqlite3.connect('database.db')
+    try:
+        connection = sqlite3.connect('database.db')
+    except Exception as err:
+        logger.error("Error {0} has occured while connecting to database.db!".format(err))
     connection.row_factory = sqlite3.Row
     logger.info("Successfully connected to the database!")
     db_connection_count += 1
     return connection
 
-# Function to get a post using its ID
+
 def get_post(post_id):
+    """
+    Function to get a post using its ID
+    """
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
